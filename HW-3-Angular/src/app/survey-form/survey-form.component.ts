70779939b7006580bfe88f3da92522783a7cb39d
Akshath Phillips <akshathphillips@icloud.com>
2023-11-12 19:19:46 -0500

    Minor fixes #2

--- HW-3-Angular/src/app/survey-form/survey-form.component.ts
+++ HW-3-Angular/src/app/survey-form/survey-form.component.ts
@@ -18,17 +18,13 @@ export class SurveyFormComponent {
   ];
 
   surveyData: any = {
-    likedMostOptions: [] // Initialize an empty array for likedMostOptions
+    likedMostOptions: []
   };
-
-  // Define properties for other Survey model properties here
 
   onLikedMostOptionChange(selected: boolean, option: string) {
     if (selected) {
-      // If the option is selected, add it to the array
       this.surveyData.likedMostOptions.push(option);
     } else {
-      // If the option is deselected, remove it from the array
       const index = this.surveyData.likedMostOptions.indexOf(option);
       if (index !== -1) {
         this.surveyData.likedMostOptions.splice(index, 1);
@@ -37,26 +33,22 @@ export class SurveyFormComponent {
   }
 
   submitForm(formData: any) {
-    // Define the API URL
     const apiUrl = 'http://localhost:8080/api/surveys';
 
-    // Define the headers for the POST request (you may need to adjust them)
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-    // Send the POST request to the API
     this.httpClient
       .post(apiUrl, formData, {headers})
       .pipe(
         catchError((error: any) => {
           console.error('Error submitting survey', error);
-          throw new Error(error); // Re-throw the error to propagate it further
+          throw new Error(error);
         })
       )
       .subscribe((response) => {
         console.log('Survey submitted successfully', response);
-        // Reset the form after successful submission if needed
         // this.surveyForm.reset();
       });
   }
